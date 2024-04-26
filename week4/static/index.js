6b2183514c39081c156ff632c3087041404dd827
jcl77777 <146434729+jcl77777@users.noreply.github.com>
2024-04-26 16:06:35 +0800

    update folders

--- week4/static/index.js
+++ week4/static/index.js
@@ -11,3 +11,35 @@ loginForm.addEventListener('submit', function(event) {
     loginForm.submit();
   }
 });
+
+
+/*JSONResponse
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("loginForm");
+  form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      const formData = new FormData(form);
+      const data = {
+          username: formData.get('username'),
+          password: formData.get('password')
+      };
+      fetch('/signin', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(data => {
+          if (data.redirect) {
+              window.location.href = data.redirect;
+          } else {
+              // Handle errors or show messages here
+              alert(data.error_message);
+          }
+      })
+      .catch(error => console.error('Error:', error));
+  });
+});
+*/
