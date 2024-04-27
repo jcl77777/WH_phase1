c976439548ff7b16239d1a9ed3a7c8f504fc053d
jcl77777 <146434729+jcl77777@users.noreply.github.com>
2024-04-27 21:19:20 +0800

    Update index.js

--- week4/static/index.js
+++ week4/static/index.js
@@ -5,41 +5,9 @@ loginForm.addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent the form without checking the box
 
   if (!termsCheckbox.checked) {
-    alert('Please check the checkbox first');
+    alert('Please check the checkbox first'); // Show an alert
   } else {
     // Submit the form
     loginForm.submit();
   }
 });
-
-
-/*JSONResponse
-document.addEventListener("DOMContentLoaded", function() {
-  const form = document.getElementById("loginForm");
-  form.addEventListener("submit", function(event) {
-      event.preventDefault();
-      const formData = new FormData(form);
-      const data = {
-          username: formData.get('username'),
-          password: formData.get('password')
-      };
-      fetch('/signin', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data)
-      })
-      .then(response => response.json())
-      .then(data => {
-          if (data.redirect) {
-              window.location.href = data.redirect;
-          } else {
-              // Handle errors or show messages here
-              alert(data.error_message);
-          }
-      })
-      .catch(error => console.error('Error:', error));
-  });
-});
-*/
